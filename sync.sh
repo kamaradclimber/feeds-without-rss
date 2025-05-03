d6b3320e4fc5c7d8f9c4cbbfece89050692c0bec
Grégoire Seux <grego_github@familleseux.net>
2025-05-03 12:42:17 +0200

    Update sync

--- sync.sh
+++ sync.sh
@@ -5,7 +5,10 @@ set -ex
 git pull origin main
 
 ruby update_feeds.rb
+git add feeds.toml
 
-git commit -a -m "Update jancovici feed source"
+if [[ "$(git diff --no-ext-diff --cached)" != "" ]]; then
+  git commit -a -m "Update jancovici feed source"
+  git push origin HEAD
+fi
 
-git push origin HEAD
